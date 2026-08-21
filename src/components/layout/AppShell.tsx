0e0d7c0abb0a98ffa2afbde7470aea2cfbe3d3a8
Cairon Felipe <caironf3@gmail.com>
2026-08-21 08:43:25 -0300

    fix: restaurar rolagem do scroll do mouse em dispositivos desktop removendo overscroll-behavior e overflow-x superfluos

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -7,13 +7,13 @@ import { BotaoFeedbackFlutuante } from '../feedback/BotaoFeedbackFlutuante';
 
 export const AppShell: React.FC = () => {
   return (
-    <div className="min-h-screen bg-graphite-900 text-vapor-100 flex w-full max-w-full overflow-x-hidden">
+    <div className="min-h-screen bg-graphite-900 text-vapor-100 flex w-full max-w-full">
       <SidebarNav />
       
-      <div className="flex-1 flex flex-col min-h-screen lg:pl-[240px] w-full max-w-full overflow-x-hidden">
+      <div className="flex-1 flex flex-col min-h-screen lg:pl-[240px] w-full max-w-full">
         <TopBar />
         
-        <main className="flex-1 p-4 pt-[80px] pb-[80px] lg:p-8 lg:pt-8 w-full max-w-5xl mx-auto overflow-x-hidden">
+        <main className="flex-1 p-4 pt-[80px] pb-[80px] lg:p-8 lg:pt-8 w-full max-w-5xl mx-auto">
           <Outlet />
         </main>
         
